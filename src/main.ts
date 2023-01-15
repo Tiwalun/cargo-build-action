100c79df02b16280a8a2f19667ff5e3f870f7538
Dominik Boehi <dominik.boehi@gmail.com>
2023-01-15 11:13:46 +0100

    Just run cargo build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const command: string = core.getInput('command')
+    core.debug(`Running command 'cargo ${command}'`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    exec.exec('cargo', [command])
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
